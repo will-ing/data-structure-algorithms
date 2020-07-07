07fc2da60a09b3b7542b08dca2351f5614a96ad6
will-ing <will-ing@github.com>
2020-07-06 20:57:03 -0700

    added doc strings

--- dsa-python/challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ dsa-python/challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -8,6 +8,16 @@ class AnimalShelter():
         self.cat = []
 
     def enqueue(self, animal, name):
+        """
+        Adds animal and name to a stack
+
+        Args:
+            animal (string): [Dog or Cat]
+            name (string): [Any]
+
+        Raises:
+            ValueError: [If not dog or cat is entered]
+        """
         if animal == 'dog':
             self.dog.append(name)
         elif animal == 'cat':
@@ -16,6 +26,15 @@ class AnimalShelter():
             raise ValueError('enter cat or dog animals')
 
     def dequeue(self, pref):
+        """
+        This removes the animal out of the shelter
+
+        Args:
+            pref (string): cat or dog
+
+        Raises:
+            ValueError: If cat or dog is not entered return error
+        """
         if pref == 'cat':
             self.cat.pop()
         elif pref == 'dog':
