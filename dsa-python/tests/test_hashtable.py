da16b39d1613dc4959849cfbad94152399a63ff5
will-ing <will-ing@github.com>
2020-09-05 13:40:56 -0700

    got most of it done, having trouble with testing

---
+++ dsa-python/tests/test_hashtable.py
@@ -0,0 +1,20 @@
+from challenges.hashtable.hashtable import Hashtable
+
+
+def test_add():
+    hashtable = Hashtable()
+    hashtable.add('tired', 1)
+    hashed_key = hashtable._hash('tired')
+    assert hashtable._buckets[hashed_key].head.data[0] == 'tired'
+
+
+def test_get():
+    hashtable = Hashtable()
+    hashtable.add('tired', 1)
+    assert hashtable.get('tired') == 1
+
+
+def test_():
+    hashtable = Hashtable()
+    hashtable.add('tired', 1)
+    assert hashtable.get('low') == 1
